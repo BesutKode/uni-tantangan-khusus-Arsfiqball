264b5c4ba77529846ba3500f2bfa311410ff31aa
Iqbal Mohammad Abdul Ghoni <iqballmags@gmail.com>
2017-02-10 08:55:30 +0700

    Add wikipedia support on osm query w/ queue

--- ctrl/osm.js
+++ ctrl/osm.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const osm = require('../src/osm');
+const async = require('async');
+const wikipedia = require('../src/wikipedia');
 
 module.exports = (cli) => {
   function osmQuery(query, opts) {
@@ -7,8 +9,34 @@ module.exports = (cli) => {
     var osmPromise = osm.request(query).then(osm.parse);
 
     if (opts.save) {
-      console.log(opts.save);
       osmPromise = osmPromise.then(osm.save);
+    }
+
+    if (opts.wikipedia) {
+      osmPromise = osmPromise.then((osmdata) => {
+        return new Promise((resolve, reject) => {
+          var q = async.queue((task, done) => {
+            wikipedia
+              .request(task.title, task.lang)
+              .then(wikipedia.save(task.lang))
+              .then(() => {
+                done();
+              });
+          }, 1);
+
+          q.drain = () => {};
+
+          var keys;
+          for (var i = 0; i < osmdata.elements.length; i++) {
+            if (osmdata.elements[i].tags && osmdata.elements[i].tags.wikipedia) {
+              keys = osmdata.elements[i].tags.wikipedia.split(':');
+              q.push({ title: keys[1], lang: keys[0] });
+            }
+          }
+
+          resolve(osmdata);
+        });
+      });
     }
 
     osmPromise
@@ -26,6 +54,7 @@ module.exports = (cli) => {
   cli.command('osm:query <query>')
     .description('Update dataset by querying to OpenStreetMap')
     .option('--save', 'Save to database')
+    .option('--wikipedia', 'Find connected wikipedia contributors')
     .action(osmQuery);
 
   /**
@@ -34,6 +63,7 @@ module.exports = (cli) => {
   cli.command('osm:queryfile <filepath>')
     .description('Update dataset by querying to OpenStreetMap using overpassql file')
     .option('--save', 'Save to database')
+    .option('--wikipedia', 'Find connected wikipedia contributors')
     .action((filepath, opts) => {
       fs.readFile(process.cwd()+'/'+filepath, (err, query) => {
         return osmQuery(query, opts);
